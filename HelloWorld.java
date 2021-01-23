6dae2066810f8ab797fda6133770c5438fd4f904
Zak19018 <64109198+Zak19018@users.noreply.github.com>
2021-01-23 18:58:19 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package shaik;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	    new HelloWorld().sayHello();
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello World!");
+    }
+}
