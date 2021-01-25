4b9b6c229a14bfe0d821b976fba4eda8829de479
Jaredshongwe <jaredshongwe@gmail.com>
2021-01-25 20:56:07 +0200

    added new print line and commented out package

--- HelloFromZak.java
+++ HelloFromZak.java
@@ -1,8 +1,9 @@
-package shaik.prove04;
+//package shaik.prove04;
 
 public class HelloFromZak {
 
     public static void main (String[] args) {
         System.out.println("Hello, my name is Zak.");
+        System.out.println("Hello Zak, my name is Jared.");
     }
 }
